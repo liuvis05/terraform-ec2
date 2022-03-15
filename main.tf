9b12b1aefb75191f9cbd907db737aac0bb6d17b4
liuvis05 <94665901+liuvis05@users.noreply.github.com>
2022-03-15 17:29:31 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ provider "aws" {
 
 }
 resource "aws_instance" "my-first-server" {
-  ami           = "ami-0e472ba40eb589f49"
+  ami           = "$"
   instance_type = "t2.micro"
   tags = {
     Name = "Ubuntu"
